63ba0b8b3797c546e94a2bbb60158dfbf309c1d2
yuzheyi <1309295630@qq.com>
2023-11-07 08:42:42 +0800

    还差一个矩阵求解器

--- test/main.cpp
+++ test/main.cpp
@@ -1,16 +1,33 @@
+// // main.cpp
+// #include <iostream>
+// #include <vector>
+// #include "FluidProperties/FluidProperties.h"
+
+// int main() {
+//     FluidProperties fluid(1.0, 2.0, 3.0, 100000.0, 1.2, 1.1, 1.1, 10.0, 300.0);
+//     std::vector<double> position = fluid.PointPosition();
+//     position[0] = position[0] + 30;
+//     double *f = &position[0];
+//     std::cout << "xPosition: " << *f << std::endl;
+
+//     return 0;
+// }
+
+
 #include <iostream>
 #include <vector>
 #include "Fvm.h"
 #include <cmath>
+
+
 /**
  * @brief The main function generates grid points, constructs faces and cells.
  * 
  * @return int 
  */
-
 int main() {
     //生成一维网格点
-    int number = 40;
+    int number = 100;
     Point mypoints[number];
     int count = 0;
     while (count < number) {
@@ -39,38 +56,35 @@ int main() {
     
 
    
-    double K[number-1][number-1] = {}; //构造系数矩阵
+    double K[number-1][number-1]; //构造系数矩阵
     double F[number-1];//构造右端项
-    
+
     /*初始化边界条件，通过修改右端项的第一行与最后一行实现,其中point[0]=2,point[number]=0*/
     {
         int i = 0;
-        double div[2]={(faces[i].position - cells[i].position),(cells[i+1].position - cells[i].position)};
-        double normal[2] = {div[0]/fabs(div[0]),div[1]/fabs(div[1])};
-        K[i][i] = -1/div[0]*normal[0] + -1/div[1]*normal[1];//系数矩阵
-        K[i][i+1] = 1/div[1]*normal[1];
-        double f_In = 1/div[0] * normal[0] * faces[i].physics;
+        double normal[2] = {(cells[i].position-faces[i].position)/fabs(cells[i].position-faces[i].position), (cells[i].position-cells[i+1].position)/fabs(cells[i].position-cells[i+1].position)};//表示通量方向
+        K[i][i] = 1/(cells[i].position-faces[i].position) * normal[0]+1/(cells[i].position-cells[i+1].position) *  normal[1];//系数矩阵
+        K[i][i+1] = -1/(cells[i].position-cells[i+1].position) * normal[1];
+        double f_In = -1/(cells[i].position-faces[i].position) * normal[0] * faces[i].physics;
         F[i] = 2*cells[i].volume - f_In; // assign a value to F[i]
     }
     {
         int i = number - 2 ;
-        double div[2]={(cells[i-1].position - cells[i].position),(faces[i+1].position - cells[i].position)};
-        double normal[2] = {div[0]/fabs(div[0]),div[1]/fabs(div[1])};
-        K[i][i] = -1/div[0]*normal[0] + -1/div[1]*normal[1];
-        K[i][i-1] = 1/div[0] * normal[0];
-        double f_Out = 1/div[1] * faces[i+1].physics;
+        double normal[2] = {(cells[i].position-cells[i-1].position)/fabs(cells[i].position-cells[i-1].position), (cells[i].position-faces[i+1].position)/fabs(cells[i].position-faces[i+1].position)};
+        K[i][i] = 1/(cells[i].position-cells[i-1].position) * normal[0] + 1/(cells[i].position-faces[i+1].position) *  normal[1];
+        K[i][i-1] = -1/(cells[i].position-cells[i-1].position) * normal[0];
+        double f_Out = -1/(cells[i].position-faces[i+1].position) * normal[1] * faces[i+1].physics;
         F[i] = 2*cells[i].volume - f_Out; // assign a value to F[i]
     }
 
 
 
     for(int i = 1; i < number-2; i++)
-    {   
-        double div[2]={(cells[i-1].position - cells[i].position),(cells[i+1].position - cells[i].position)};
-        double normal[2] = {div[0]/fabs(div[0]),div[1]/fabs(div[1])};
-        K[i][i] =  -1/div[0]*normal[0] + -1/div[1]*normal[1];
-        K[i][i-1] = 1/div[0] * normal[0];
-        K[i][i+1] = 1/div[1] * normal[1];
+    {
+        double normal[2] = {(cells[i].position-cells[i-1].position)/fabs(cells[i].position-cells[i-1].position), (cells[i].position-cells[i+1].position)/fabs(cells[i].position-cells[i+1].position)};
+        K[i][i] = 1/(cells[i].position-cells[i-1].position) * normal[0] + 1/(cells[i].position-cells[i+1].position) *  normal[1];
+        K[i][i-1] = -1/(cells[i].position-cells[i-1].position) * normal[0];
+        K[i][i+1] = -1/(cells[i].position-cells[i+1].position) * normal[1];
         F[i] = 2*cells[i].volume; // assign a value to F[i]
     }
 
@@ -89,14 +103,6 @@ int main() {
 
 
     //     }
-    for (int i = 0; i < number-1; i++) {
-        for (int j = 0; j < number-1; j++) {
-            std::cout << K[i][j] << "\t"; // 使用制表符分隔元素
-        }
-        std::cout << std::endl; // 在每行结束时换行
-    }
-    std::cout << "Press any key to exit...";
-    std::cin.get(); // 等待用户输入
 
     return 0;
 }
